600d05d63c70f59c30066de8ec641eb4cff5dac6
Elevn <752944885@qq.com>
2018-03-20 22:56:25 +1100

    Web version0.1.1

--- flaskImplementation/flaskImplementation.py
+++ flaskImplementation/flaskImplementation.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request, url_for, redirect
 import config
 from exts import db
 from models import User
@@ -20,14 +20,35 @@ def login():
     if request.method == 'GET':
         return render_template('login.html')
     else:
-        return render_template('dashboard.html')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        password_check = User.query.filter(User.username == username).first()
+        if password == password_check:
+            return redirect(url_for('dashboard'))
+        else:
+            return 'Wrong password'
 
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
         return render_template('register.html')
     else:
-        return render_template('dashboard.html')
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter(User.email == email).first()
+        if user:
+            return 'User existed'
+        else:
+            if password1 != password2:
+                return 'Password confirmation error'
+            else:
+                user = User(username=username, email=email, password=password1)
+                db.session.add(user)
+                db.session.commit()
+                return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run()
