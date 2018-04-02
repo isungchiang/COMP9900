e17e651114bc958ffe089b324c79f3f3621a2a46
hanxiaochen1019 <hanxiaochen1019@gmail.com>
2018-04-02 12:20:45 +0800

    marketwatch news crawler
    marketwatch.py  is used to crawl urls
    content_crawler.py is used to crawl content

---
+++ content_crawler.py
@@ -0,0 +1,144 @@
+from splinter import Browser
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import WebDriverException
+import pandas
+import time
+import random
+import threading
+import os
+
+RUNNING_THREADS = 20
+SLEEP_PERIOD = 0.3
+
+##os.rmdir("C:\\Users\Oscar\Desktop\stockdata")
+prof = {}
+prof['browser.download.manager.showWhenStarting'] = 'false'
+prof['browser.helperApps.alwaysAsk.force'] = 'false'
+prof['browser.download.dir'] = "/home/ohan/Desktop/stockdata"
+prof['browser.download.folderList'] = 2
+prof['browser.helperApps.neverAsk.saveToDisk'] = 'text/csv, application/csv, text/html,application/xhtml+xml,application/xml, application/octet-stream, application/pdf, application/x-msexcel,application/excel,application/x-excel,application/excel,application/x-excel,application/excel, application/vnd.ms- excel,application/x-excel,application/x-msexcel,image/png,image/jpeg,text/html,text/plain,application/msword,application/xml,application/excel,text/x-c'
+prof['browser.download.manager.useWindow'] = 'false'
+prof['browser.helperApps.useWindow'] = 'false'
+prof['browser.helperApps.showAlertonComplete'] = 'false'
+prof['browser.helperApps.alertOnEXEOpen'] = 'false'
+prof['browser.download.manager.focusWhenStarting']= 'false'
+prof['webdriver.load.strategy'] =  'unstable'
+prof['browser.load.strategy'] =  'unstable'
+
+code_list = []
+for csvs in (os.listdir('./')):
+    if 'URL' not in csvs:
+        continue
+    if 'txt' in csvs:
+        continue
+    else:
+        code_list.append(csvs)
+code_list.sort()
+
+with open('./404_URL_list.txt', 'r') as f:
+    list_404 = f.readlines()
+    f.close()
+# print('not found list: ', list_404)
+#
+with open('./checked_URL_list.txt', 'r') as f:
+    checked_list = f.readlines()
+    f.close()
+
+def action(csv):
+    urls = pandas.read_csv(csv)
+    code_list = []
+    title_list = []
+    url_list = []
+    date_list = []
+    content_list = []
+    listening_list = []
+    print('start: ' + csv + ' ' + ' ' + 'len: ' + str(len(urls['code'])))
+    for i in range(0, len(urls['code'])):
+        url = (urls['url'][i])
+
+        with Browser('firefox', profile_preferences=prof) as new_browser:
+            new_browser.visit(url)
+            if new_browser.is_element_present_by_id('article-body', wait_time=5):
+                body = str(new_browser.find_by_id('article-body').value)
+                body = body.replace('\n', ' ')
+            else:
+                body = 'N/A'
+            if new_browser.is_element_present_by_id('published-timestamp', wait_time=5):
+                date = new_browser.find_by_id('published-timestamp')
+                date = str(date.value).replace('Published: ', '')
+            else:
+                date = 'N/A'
+            # print('date:',date, 'body:',body)
+            if new_browser.is_element_not_present_by_css('.fyre-livecount', wait_time=5):
+                listening = 'N/A'
+            else:
+                if(new_browser.find_by_css('.fyre-livecount')):
+                    listening = new_browser.find_by_css('.fyre-livecount')
+                    listening = str(listening.value).split()[0]
+
+                else:
+                    listening = 'N/A'
+
+            code_list.append(urls['code'][i])
+            title_list.append(urls['title'][i])
+            url_list.append(str(urls['url'][i]))
+            date_list.append(date)
+            listening_list.append(listening)
+            content_list.append(body)
+
+
+    code = str(csv).replace('URL', '')
+    code = code.replace('.csv', '')
+    pandas.DataFrame({'code': code_list, 'url': url_list, 'title': title_list, 'date':date_list, 'listening':listening_list, 'content': content_list}).to_csv(
+        './marketwatch_content/' + 'ccontent_' + code + ".csv", index=False, sep=',')
+    print('|'+code+'|' + ' finish')
+    with open('./checked_URL_list.txt', 'a') as f:
+        f.writelines(csv+'\n')
+        f.close()
+#
+#
+#
+#         print(code + 'finish')
+#         print(len(code_index_list), len(url_list), len(title_list))
+#
+#         # print(len(url_list), len(title_list), len(date_list), len(context_list), len(listening_list))
+#         # print((url_list), (title_list), (date_list), (context_list), (listening_list))
+#         # pandas.DataFrame({'url': url_list, 'title': title_list, 'date': date_list, 'context': context_list, 'listening': listening_list}).to_csv('./marketwatch/'+code+".csv", index=False, sep=',')
+#         # running_list.pop()
+#
+#
+#
+running_list = []
+
+while code_list:
+    # print('rest codes', len(codes_list))
+    for threads in running_list:
+        if threads.is_alive():
+            pass
+        else:
+            running_list.remove(threads)
+    while len(running_list) < RUNNING_THREADS:
+        if not code_list:
+            break
+        csv = code_list.pop(0)+'\n'
+        if csv in checked_list:
+            print(csv+'checked')
+            # print('rest:', len(codes_list))
+            continue
+        if csv in list_404:
+            print(csv+'not found')
+            continue
+        else:
+            csv = str(csv).replace('\n', '')
+            print('rest codes', len(code_list))
+            t = threading.Thread(target=action, args=(csv,), name=csv)
+            t.start()
+            running_list.append(t)
+            checked_list.append(csv)
+    time.sleep(3)
+
+
+
+
+
--- marketwatch.py
+++ marketwatch.py
@@ -1,12 +1,14 @@
 from splinter import Browser
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import WebDriverException
 import pandas
 import time
 import random
 import threading
 
-SCROLL_TIMES = 100
-RUNNING_THREADS = 20
+SCROLL_TIMES = 200
+RUNNING_THREADS = 10
 SLEEP_PERIOD = 0.3
 
 ##os.rmdir("C:\\Users\Oscar\Desktop\stockdata")
@@ -21,6 +23,10 @@ prof['browser.helperApps.useWindow'] = 'false'
 prof['browser.helperApps.showAlertonComplete'] = 'false'
 prof['browser.helperApps.alertOnEXEOpen'] = 'false'
 prof['browser.download.manager.focusWhenStarting']= 'false'
+prof['webdriver.load.strategy'] =  'unstable'
+prof['browser.load.strategy'] =  'unstable'
+prof['driver.set_page_load_timeout']  = 10000000
+prof['browser.set_page_load_timeout']  = 10000000
 
 
 ## delete previous csv
@@ -30,43 +36,54 @@ codes_list = []
 with open('US_codes.txt', 'r') as f:
     codes_list = f.readlines()
     f.close()
+print(codes_list)
 #
-# with open('CNN_not_found.txt', 'r') as f:
-#     list_404 = f.readlines()
-#     f.close()
+with open('./marketwatch/404_list.txt', 'r') as f:
+    list_404 = f.readlines()
+    f.close()
 # print('not found list: ', list_404)
 #
 with open('./marketwatch/checked_list.txt', 'r') as f:
     checked_list = f.readlines()
     f.close()
-# print('checked list: ', checked_list)
+print('checked list: ', checked_list)
 global running_list
 running_list = []
 
-
+# executable_path = {'executable_path':'/opt/google/chrome/google-chrome'}
 
 
 def action(code):
     global running_list
-    global date_list, context_list, url_list, title_list, listening_list
+    # date_list, context_list, url_list, title_list, listening_list
     # global csv_dictionary
     # csv_dictionary = {'url':'', 'date':'', 'context':'', 'title':'', 'listening':''}
-    with Browser('firefox',profile_preferences=prof) as browser:
+    # with Browser('chrome', **executable_path) as browser:
+    with Browser('firefox', profile_preferences=prof)as browser:
+
         # Visit URL
 
         # fast test
-        # code = 'AA'
-
+        # code = 'AAPL'
+        # browser.driver.set_page_load_timeout(10000000)
         url = 'https://www.marketwatch.com/investing/stock/'+code+'/news'
-        browser.visit(url)
+        try:
+            browser.visit(url)
+        except WebDriverException:
+            print(code+': visit failed')
+            with open('./marketwatch/404_list.txt', 'a') as f:
+                f.writelines(code+'\n')
+                f.close()
+            return
+        # browser.driver.set_page_load_timeout(10000000)
         if browser.is_element_not_present_by_css('.headlinewrapper') or browser.is_element_present_by_text('No News currently available for '):
             # running_list.pop()
             with open('./marketwatch/checked_list.txt', 'a') as f:
-                f.writelines(code)
+                f.writelines(code+'\n')
                 f.close()
             return
         element = browser.find_by_css('.headlinewrapper').first
-        element.mouse_over()
+        # element.mouse_over()
 
         print(code+'crawling')
         scroll_times = SCROLL_TIMES
@@ -79,66 +96,80 @@ def action(code):
         except:
             pass
 
+        code_index_list = []
         url_list = []
         title_list = []
-        date_list = []
-        context_list = []
-        listening_list = []
+        # date_list = []
+        # context_list = []
+        # listening_list = []
+
         if browser.is_element_present_by_css('.headlinewrapper>ol>li>div>p>a'):
+
             for hrefs in browser.find_by_css('.headlinewrapper>ol>li>div>p>a'):
                 if hrefs['href'] in url_list:
                     continue
 
                 if hrefs['href'] != None:
                     if 'marketwatch.' not in str(hrefs['href']):
-                        print(hrefs['href'])
+                        # print(hrefs['href'])
                         continue
-
+                    # print(hrefs['href'])
                     if hrefs['text'] != None:
                         title = str(hrefs['text'])
                     else:
                         title = 'N/A'
 
-                    with Browser('firefox', profile_preferences=prof) as new_browser:
-                        new_browser.visit(str(hrefs['href']))
-                        if new_browser.is_element_present_by_id('article-body'):
-                            body = str(new_browser.find_by_id('article-body').value)
-                            body = body.replace('\n', ' ')
-                        else:
-                            body = 'N/A'
-                        if new_browser.is_element_present_by_id('published-timestamp'):
-                            date = new_browser.find_by_id('published-timestamp')
-                            date = str(date.value).replace('Published: ', '')
-                        else:
-                            date = 'N/A'
-                        # print('date:',date, 'body:',body)
-                        if(new_browser.find_by_css('.fyre-stream-livecount')):
-                            listening = new_browser.find_by_css('.fyre-stream-livecount')
-                            listening = str(listening.value).split()[0]
+                    code_index_list.append(code)
+                    url_list.append(hrefs['href'])
+                    title_list.append(title)
 
-                        else:
-                            listening = 'N/A'
+                    # with Browser('firefox', profile_preferences=prof) as new_browser:
+                    #     new_browser.visit(str(hrefs['href']))
+                    #     if new_browser.is_element_present_by_id('article-body'):
+                    #         body = str(new_browser.find_by_id('article-body').value)
+                    #         body = body.replace('\n', ' ')
+                    #     else:
+                    #         body = 'N/A'
+                    #     if new_browser.is_element_present_by_id('published-timestamp'):
+                    #         date = new_browser.find_by_id('published-timestamp')
+                    #         date = str(date.value).replace('Published: ', '')
+                    #     else:
+                    #         date = 'N/A'
+                    #     # print('date:',date, 'body:',body)
+                    #     if new_browser.is_element_not_present_by_css('.fyre-livecount'):
+                    #         listening = 'N/A'
+                    #     else:
+                    #         if(new_browser.find_by_css('.fyre-livecount')):
+                    #             listening = new_browser.find_by_css('.fyre-livecount')
+                    #             listening = str(listening.value).split()[0]
+                    #
+                    #         else:
+                    #             listening = 'N/A'
+                    #
+                    #     url_list.append(str(hrefs['href']))
+                    #     title_list.append(title)
+                    #     date_list.append(date)
+                    #     context_list.append(body)
+                    #     listening_list.append(listening)
 
-                        url_list.append(str(hrefs['href']))
-                        title_list.append(title)
-                        date_list.append(date)
-                        context_list.append(body)
-                        listening_list.append(listening)
+
 
         print(code + 'finish')
-        print(len(url_list), len(title_list), len(date_list), len(context_list), len(listening_list))
+        print(len(code_index_list), len(url_list), len(title_list))
+        pandas.DataFrame({'code': code_index_list, 'url': url_list, 'title': title_list}).to_csv('./marketwatch/'+ 'URL_' + code + ".csv", index=False, sep=',')
+        # print(len(url_list), len(title_list), len(date_list), len(context_list), len(listening_list))
         # print((url_list), (title_list), (date_list), (context_list), (listening_list))
-        pandas.DataFrame({'url': url_list, 'title': title_list, 'date': date_list, 'context': context_list, 'listening': listening_list}).to_csv('./marketwatch/'+code+".csv", index=False, sep=',')
+        # pandas.DataFrame({'url': url_list, 'title': title_list, 'date': date_list, 'context': context_list, 'listening': listening_list}).to_csv('./marketwatch/'+code+".csv", index=False, sep=',')
         # running_list.pop()
     with open('./marketwatch/checked_list.txt', 'a') as f:
-        f.writelines(code)
+        f.writelines(code+'\n')
         f.close()
 
 
 while codes_list:
 
 
-    print('rest codes', len(codes_list))
+    # print('rest codes', len(codes_list))
     for threads in running_list:
         if threads.is_alive():
             pass
@@ -150,13 +181,20 @@ while codes_list:
         code = codes_list.pop(0)
         if code in checked_list:
             print(code+'checked')
+            # print('rest:', len(codes_list))
+            continue
+        if code in list_404:
+            print(code+'not found')
             continue
         else:
+            code = str(code).replace('\n', '')
+            print('rest codes', len(codes_list))
             t = threading.Thread(target=action, args=(code,), name=code)
             t.start()
             running_list.append(t)
             checked_list.append(code)
     time.sleep(3)
+
 
 
 
